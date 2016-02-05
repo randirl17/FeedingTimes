fa267dafc12ba1681ec13c403fe9e7d8ae5eebc5
Randi <randirl17@gmail.com>
2016-02-04 22:47:17 -0600

    munge data, calculate total feeding times

--- nursing.py
+++ nursing.py
@@ -3,16 +3,51 @@ Read in data from csv and determine patterns for each month.
 """
 
 import sys
+import numpy as np
 import pandas as pd
+from pandas import DataFrame
+from datetime import datetime
 
+def convmmss(mmss):
+# converts column of ['mm','ss'] lists to array of mm.mm data points
+    minlist = []
+    for value in mmss:
+        if len(value) == 1:
+            value = [float('NaN'),float('NaN')]
+        else:
+            if int(value[1]) < 10:
+                value[1] = value[1] + '0'
+        minutes = float(value[0]) + float(value[1])/60.
+        minlist.append(minutes)
+    minarr = np.array(minlist)
+    return minarr
+        
 def munge():
     df = pd.read_csv('NursingData.csv')
 #Nursing app returns data by tab in the app, i.e. nursing data first, then diapers, sleep, and pumping are appended, rather than inserting data into the correct columns.  Need to consolidate data by date first.
+
+#Column headers need to be unpadded.
     headers = list(df.columns.values)
     newhead = []
     for name in headers:
         newhead.append(name.strip())
+    df.columns = newhead
     
+    df['time'] = pd.to_datetime(df['time'])
+
+#Need to convert mm:ss entries to mm.mm
+    leftmin = convmmss(df['Left'].str.split(':'))
+    rightmin = convmmss(df['Right'].str.split(':'))
+
+#Total feeding time = left + right if nursing, default 5 min if bottle fed
+    botarr = np.array(df['Bottle'])
+    bottime = np.where(botarr == ' Bottle-Pump',5.,0.)
+    totfeed = leftmin + rightmin + bottime
+    df.loc[:,'Total Feed'] = pd.Series(totfeed, index = df.index)
+
+
+    
+    ordered = df.sort_values('time')
     
 return
 
