0f66eb68279bb55f75a200dae45e5a568a7bda2b
Randi <randirl17@gmail.com>
2016-02-27 01:19:11 -0600

    power law fit

--- NursingPlots.py
+++ NursingPlots.py
@@ -28,7 +28,18 @@ Time sleeping in one day vs time eating in one day (correlated?)
 Do naps that immediately follow nursing last longer than ones that don't? (Time since last nurse vs. length of nap)
 Frequency of bottle feedings per month (have bottle feedings become more rare with age?)
 
-make keys of month (0,1,2...) and week to .groupby() and perform summary stats on 
+make keys of month (0,1,2...) and week to .groupby() and perform summary stats on
+
+Does the baby tends to nap longer immediately following a feeding?
+
+Do the number of naps or cumulative napping time have an impact on the number/frequency of nighttime wakings or total sleep time?
+
+Has the baby shown changes in patterns of feedings/sleeping as time progresses?
+
+Does the baby tend to nurse on both sides more often (thus consuming more milk at a time) as the months have passed?
+
+time vs total feed?  total feed per day vs date?
+
 """
 
 def main():
